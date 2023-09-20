9d41a7907e9095404b5faaa7dec5293b3bc05f54
Rick Verbon <R.Verbon@tilburguniversity.edu>
2023-09-20 14:30:36 +0200

    First commit

---
+++ products/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from products.models import Product
+# Create your views here.
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "products/product_list.html"
+    context_object_name = "products"
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "products/product_detail.html"
+    context_object_name = "product"
